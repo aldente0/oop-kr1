9e6fb6c1ab09fcb1396d5e3b99cdd84ed0e5d0f9
Алексей Ложкин <leha.lozhkin.02@mail.ru>
2023-12-23 12:31:33 +0300

    fix unchanging rect size

--- oop-kr1/RandomRect.h
+++ oop-kr1/RandomRect.h
@@ -2,8 +2,6 @@
 #include "Rect.h"
 class RandomRect : public Rect
 {
-private:
-	RECT rect;
 public:
 	RandomRect(int left, int top, int right, int bottom, int color, int fillStyle)
 		: Rect(left, top, right, bottom, color, fillStyle) {};
